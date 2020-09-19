bb98317d10226ea855417a9949cc5b41708b3b4f
Fangxin Wang <2681086480@qq.com>
2020-09-19 22:00:08 +0800

    jdbc整合

---
+++ springboot-04-data/src/main/java/com/kuang/controller/JDBCController.java
@@ -0,0 +1,55 @@
+package com.kuang.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class JDBCController {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    // 查询数据库的所有信息
+    // 没有实体类，获取数据库的东西，怎么获取？ Map
+    @GetMapping("/userList")
+    public List<Map<String,Object>> userList(){
+        String sql = "select * from user";
+        List<Map<String, Object>> list_maps = jdbcTemplate.queryForList(sql);
+        return list_maps;
+    }
+
+    @GetMapping("/addUser")
+    public String addUser(){
+        String sql = "insert into user(uid,uname,usex) values(1,'小明',0)";
+        // 自动帮我们提交了，不需要我们自己提交
+        jdbcTemplate.update(sql);
+        return "update-ok";
+    }
+
+    @GetMapping("/updateUser/{id}")
+    public String updateUser(@PathVariable("id") int id){
+        String sql = "update user set uid=?,uname=? where uid="+id;
+        // 封装
+        Object[] objects = new Object[2];
+        objects[0]=2;
+        objects[1]="小花";
+
+        jdbcTemplate.update(sql,objects);
+        return "updateUser-ok";
+    }
+
+    @GetMapping("/deleteUser/{id}")
+    public String deleteUser(@PathVariable("id") int id){
+        String sql = "delete from user where uid = ?";
+        // 自动帮我们提交了，不需要我们自己提交
+        jdbcTemplate.update(sql,id);
+        return "deleteUser-ok";
+    }
+
+}
--- springboot-04-data/src/test/java/com/kuang/Springboot04DataApplicationTests.java
+++ springboot-04-data/src/test/java/com/kuang/Springboot04DataApplicationTests.java
@@ -19,6 +19,15 @@ class Springboot04DataApplicationTests {
         //查看一下默认的数据源：class com.zaxxer.hikari.HikariDataSource
         System.out.println(dataSource.getClass());
 
-    }
+        // 获得数据库链接
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
 
+        // xxxx Template:SpringBoot已经配置好的模版bean，拿来即用
+        // JDBC Template
+        // radis Template
+
+        // 关闭链接
+        connection.close();
+    }
 }
