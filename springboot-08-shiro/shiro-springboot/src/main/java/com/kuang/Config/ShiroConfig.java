25b63b5c9e75ef65174170b9abec1d05fe5dcef0
Fangxin Wang <2681086480@qq.com>
2020-09-21 09:57:15 +0800

    Shiro

--- springboot-08-shiro/shiro-springboot/src/main/java/com/kuang/Config/ShiroConfig.java
+++ springboot-08-shiro/shiro-springboot/src/main/java/com/kuang/Config/ShiroConfig.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @Configuration
 public class ShiroConfig {
     // 创建realm对象，需要自定义类
@@ -31,6 +34,21 @@ public class ShiroConfig {
         ShiroFilterFactoryBean bean = new ShiroFilterFactoryBean();
         // 设置安全管理器
         bean.setSecurityManager(defaultWebSecurityManager);
+
+        // 添加shiro的内置过滤器
+        /**
+         * anon:无需认证就可以访问
+         * authc：必须认证了才可以访问
+         * user:必须拥有记住我功能才能用
+         * perms：拥有对某个资源的权限才能访问
+         * roles：拥有某个角色权限才能访问
+         */
+
+        Map<String,String> filterMap = new LinkedHashMap<>();
+        filterMap.put("/user/add","authc");
+        filterMap.put("/user/update","authc");
+
+        bean.setFilterChainDefinitionMap(filterMap);
         return bean;
     }
 }
